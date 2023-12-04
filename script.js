d181729a286e9b1545495824ac9e6a3954b23fab
janmar6 <janmar6@gmail.com>
2023-12-04 19:22:18 +0200

    search field, default links more better delete button

--- script.js
+++ script.js
@@ -1,94 +1,201 @@
-	let deleteMode = false;
+let deleteMode = false;
 
-	// Load buttons from localStorage when the page loads
-	document.addEventListener("DOMContentLoaded", function() {
-		loadButtons();
+// Laadi nupud localStorage'ist, kui leht laaditakse
+document.addEventListener("DOMContentLoaded", function() {
+	loadButtons();
+});
+// Reset search field value on page load
+window.onload = function() {
+    var searchField = document.getElementById("searchBox");
+    searchField.value = "";
+    searchField.focus();
+};
 
+
+function createButton() {
+	var btnText = prompt("Sisestage nupu tekst:");
+	if (!btnText) { return; }
+	var btnLink = prompt("Sisestage nupu link:");
+	if (!btnLink) { return; }
+
+	var button = document.createElement("a");
+	button.setAttribute("href", btnLink);
+	button.textContent = btnText;
+	button.classList.add("button");
+
+	document.getElementById("button-container").appendChild(button);
+
+	// Salvesta nupu andmed localStorage'i
+	saveButton(btnText, btnLink);
+}
+
+function changeColorAndEnableDelete() {
+	var buttons = document.querySelectorAll("#button-container a");
+	var deletebutton = document.querySelector("#delete");
+
+	if (deleteMode) {
+	    deletebutton.classList.remove("deletepressed");
+		buttons.forEach(function(btn) {
+			btn.classList.remove("deleteMe");
+		});
+		deleteMode = false;
+		return;
+	}
+	deletebutton.classList.add("deletepressed");
+
+	buttons.forEach(function(btn) {
+		btn.classList.add("deleteMe");
 	});
 
-	function createButton() {
-		var btnText = prompt("Enter button text:");
-		if (!btnText) {return}
-		var btnLink = prompt("Enter button link:");
-		if (!btnLink) {return}
+	// Aktiveeri kustutamise funktsioon
+	buttons.forEach(function(btn) {
+		btn.addEventListener("click", function() {
+			if (deleteMode) {
+				event.preventDefault(); // Väldi lingi käitumist
+				this.remove();
+				updateButtons(this.text); // Uuenda nuppude andmeid pärast kustutamist
+			};
+		});
+	});
+	deleteMode = true; // Luba kustutamise režiim
+}
 
+function saveButton(text, link) {
+	var buttons = JSON.parse(localStorage.getItem("buttons")) || [];
+	buttons.push({ text: text, link: link });
+	localStorage.setItem("buttons", JSON.stringify(buttons));
+}
+
+function updateButtons(buttonText) {
+	var buttons = JSON.parse(localStorage.getItem("buttons")) || [];
+
+	// Leia indeks tekstiga nupust
+	var indexToRemove = buttons.findIndex(function(btn) {
+		return btn.text === buttonText;
+	});
+
+	if (indexToRemove !== -1) {
+		buttons.splice(indexToRemove, 1); // Eemalda nupu andmed
+		localStorage.setItem("buttons", JSON.stringify(buttons)); // Uuenda localStorage'i
+	}
+}
+var defaultButtons = [];
+
+defaultButtons.push({
+    text: "YouTube 📺",
+    link: "https://youtube.com"
+});
+
+defaultButtons.push({
+    text: "LinkedIn 🔗",
+    link: "https://linkedin.com"
+});
+
+defaultButtons.push({
+    text: "DuckDuckGo 🔍",
+    link: "https://duckduckgo.com"
+});
+
+defaultButtons.push({
+    text: "GitHub 💻",
+    link: "https://github.com"
+});
+
+defaultButtons.push({
+    text: "Codecademy 💡",
+    link: "https://www.codecademy.com/"
+});
+
+defaultButtons.push({
+    text: "W3Schools 🖥️",
+    link: "https://www.w3schools.com/"
+});
+
+defaultButtons.push({
+    text: "Coursera 🎓",
+    link: "https://www.coursera.org/"
+});
+
+defaultButtons.push({
+    text: "Wolfram Alpha 🧮",
+    link: "https://www.wolframalpha.com/"
+});
+
+defaultButtons.push({
+    text: "LeetCode 💻",
+    link: "https://leetcode.com/"
+});
+
+
+function loadButtons() {
+	var buttons = JSON.parse(localStorage.getItem("buttons")) || [];
+
+	// default nupud, kui web page esimest korda või tühjana laeb
+	if (buttons.length === 0) {
+		buttons = defaultButtons;
+		buttons.forEach(function(btn) {
+			saveButton(btn.text, btn.link);
+		})
+	};
+
+	buttons.forEach(function(btn) {
 		var button = document.createElement("a");
-		button.setAttribute("href", btnLink);
-		button.textContent = btnText;
+		button.setAttribute("href", btn.link);
+		button.textContent = btn.text;
+
+		// Lisa "button" klass loodud nupule
 		button.classList.add("button");
 
+		// Create an element for the Font Awesome icon
+		var icon = document.createElement("i");
+		icon.classList.add("fas", btn.icon); // Assuming Font Awesome icons are in "fas" category
+
+		// Append the icon to the button's text
+		button.appendChild(icon);
+
 		document.getElementById("button-container").appendChild(button);
+	});
+}
+// Function to filter and display buttons based on search input
+function filterButtons() {
+    var searchInput = document.getElementById("searchBox").value.toLowerCase();
+    var buttons = document.querySelectorAll("#button-container a");
 
-		// Save button data to localStorage
-		saveButton(btnText, btnLink);
-	}
+	buttons.forEach(function(btn) {
+        var buttonText = btn.textContent.toLowerCase();
+        if (buttonText.includes(searchInput)) {
+            btn.classList.remove("hide"); // Remove hide class
+        } else {
+            btn.classList.add("hide"); // Add hide class
+        }
+    });}
 
-	function changeColorAndEnableDelete() {
-		var buttons = document.querySelectorAll("#button-container a");
-		if (deleteMode) {
-			deleteMode = false;
-			buttons.forEach(function (btn) {
-				btn.classList.remove("deleteMe");
-			});
-			return
-		}
-		deleteMode = true; // Enable delete mode
-		buttons.forEach(function (btn) {
-			btn.classList.add("deleteMe");
-		});
+// Attach an event listener to the search box input event
+document.getElementById("searchBox").addEventListener("input", filterButtons);
 
+// function filterButtons() {
+//     var searchInput = document.getElementById("searchBox").value.toLowerCase();
+//     var visibleButtons = document.querySelectorAll("#button-container a:not(.hide)");
+//
+//     visibleButtons.forEach(function(btn) {
+//         var buttonText = btn.textContent.toLowerCase();
+//         if (buttonText.includes(searchInput)) {
+//             btn.classList.remove("hide"); // Remove hide class
+//         } else {
+//             btn.classList.add("hide"); // Add hide class
+//         }
+//     });
+// }
 
-		// console.log(buttons);
-		buttons.forEach(function (btn) {
+// Event listener for 'Enter' key press in the search box
+document.getElementById("searchBox").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        var visibleButtons = document.querySelectorAll("#button-container a:not(.hide)");
+        if (visibleButtons.length === 1) {
+            var link = visibleButtons[0].getAttribute("href");
+	    window.location.href = link;
+            // window.open(link, "_blank");
+        }
+    }
+});
 
-			console.log(btn);
-			btn.addEventListener("click", function () {
-				console.log("button was clicked");
-				if (deleteMode) {
-					console.log("delete mode is on");
-
-					event.preventDefault(); // Prevent link behavior
-					console.log(this.text);
-					this.remove();
-					updateButtons(this.text); // Update the stored buttons after deletion
-				};
-			})
-			console.log(btn)
-		});
-	}
-
-	function saveButton(text, link) {
-		var buttons = JSON.parse(localStorage.getItem("buttons")) || [];
-        console.log(buttons)
-		buttons.push({text: text, link: link});
-		localStorage.setItem("buttons", JSON.stringify(buttons));
-	}
-
-	function updateButtons(buttonText) {
-		var buttons = JSON.parse(localStorage.getItem("buttons")) || [];
-
-		// Find the index of the button with the specified text
-		var indexToRemove = buttons.findIndex(function (btn) {
-			return btn.text === buttonText;
-		});
-
-		if (indexToRemove !== -1) {
-			buttons.splice(indexToRemove, 1); // Remove the button data
-			localStorage.setItem("buttons", JSON.stringify(buttons)); // Update localStorage
-		}
-	}
-
-	function loadButtons() {
-		var buttons = JSON.parse(localStorage.getItem("buttons")) || [];
-
-		buttons.forEach(function (btn) {
-			var button = document.createElement("a");
-			button.setAttribute("href", btn.link);
-			button.textContent = btn.text;
-
-			// Add the "button" class to the newly created button
-			button.classList.add("button");
-
-			document.getElementById("button-container").appendChild(button);
-		});
-	}
