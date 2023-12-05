697486a4d57be8ed8144d9a4f85ed403a24e71e3
Aleksei Ganyukov <aleksei.ganyukov.student@gmail.com>
2023-12-05 13:09:14 +0200

    m

---
+++ lang.js
@@ -0,0 +1,32 @@
+const language = {
+    en: {
+        ///settings
+        settings_h: 'Settings',
+        settings_h2: 'Text size',
+        switchLang: 'Switch to estonian',
+
+        ///home
+        nav1: 'Home',
+        nav2: 'TU',
+        nav3: 'Settings',
+        h_h1: 'Welcome ',
+        m_h2: 'Choose where to go',
+
+        // More translations
+    },
+    et: {
+        ///settings
+        settings_h: 'Sätted',
+        settings_h2: 'Teksti suurus',
+        switchLang: 'Vaheta inglise keelele',
+
+        ///home
+        nav1: 'Koduleht',
+        nav2: 'TÜ',
+        nav3: 'Sätted',
+        h_h1: 'Welcome to the University of Tartu page for visually impaired people',
+        m_h2: 'Vali leht, kuhu soovid minna',
+
+        // More translations
+    }
+};
---
+++ settings.js
@@ -0,0 +1,88 @@
+///
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Load saved settings
+    loadSettings();
+
+    // Event listeners for settings
+    document.getElementById('lightMode').addEventListener('click', () => setTheme('light'));
+    document.getElementById('darkMode').addEventListener('click', () => setTheme('dark'));
+    document.getElementById('textSizeSlider').addEventListener('input', setTextSize);
+});
+
+function setTheme(mode) {
+    document.body.className = mode === 'dark' ? 'dark-mode' : 'light-mode';
+    localStorage.setItem('theme', mode);
+    propagateSettings();
+}
+
+function setTextSize() {
+    const size = document.getElementById('textSizeSlider').value;
+    document.documentElement.style.fontSize = size + 'px';
+    localStorage.setItem('textSize', size);
+    propagateSettings();
+}
+
+
+
+
+function switchLanguage() {
+  const currentLang = localStorage.getItem('language') === 'en' ? 'et' : 'en';
+  localStorage.setItem('language', currentLang);
+  updateTexts(currentLang);
+  propagateSettings();
+}
+
+function updateTexts(lang) {
+  for (const key in language[lang]) {
+      const element = document.getElementById(key);
+      if (element) {
+          element.textContent = language[lang][key];
+      }
+  }
+}
+
+
+document.getElementById('switchLang').addEventListener('click', switchLanguage);
+
+
+
+function loadSettings() {
+    const savedTheme = localStorage.getItem('theme');
+    const savedTextSize = localStorage.getItem('textSize');
+
+    if (savedTheme) {
+        document.body.className = savedTheme === 'dark' ? 'dark-mode' : 'light-mode';
+    }
+    if (savedTextSize) {
+        document.documentElement.style.fontSize = savedTextSize + 'px';
+        document.getElementById('textSizeSlider').value = savedTextSize;
+    }
+
+  const savedLang = localStorage.getItem('language') || 'en';
+  updateTexts(savedLang);
+
+  if (document.getElementById('switchLang')) {
+      document.getElementById('switchLang').textContent = language[savedLang].switchLang;
+  }
+}
+
+
+
+function propagateSettings() {
+    // Send a message to other tabs to update settings
+    localStorage.setItem('updateSettings', Date.now());
+}
+
+// Listener for settings update in other tabs
+window.addEventListener('storage', (event) => {
+    if (event.key === 'updateSettings') {
+        loadSettings();
+    }
+    if (event.key === 'language') {
+        updateTexts(localStorage.getItem('language'));
+  }
+});
+
+
